401d5459c078c680daa363e6e83d79a92bb5dd89
Rishal <Rishal.mhd009@gmail.com>
2023-05-31 20:04:57 +0530

    commit check

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,18 @@ import Homepage from "./pages/Home";
 import EventsPage from "./pages/Events";
 import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
 
 const router = createBrowserRouter([
   { path: "/", element: <Homepage /> },
   { path: "/events", element: <EventsPage /> },
-  { path: "events/:eventId", element: <EventDetailPage /> },
+  { path: "/events/:eventId", element: <EventDetailPage /> },
   { path: "/events/new", element: <NewEventPage /> },
+  {path: '/events/:eventId/edit', element: <EditEventPage />}
 ]);
 
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />
 }
 
 export default App;
