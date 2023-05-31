be05ecdc99244668c452efaf83ccc7383872af58
Rishal <Rishal.mhd009@gmail.com>
2023-05-31 19:58:18 +0530

    commit check

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,15 @@
-import { RouterProvider, createBrowserRouter} from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Homepage from "./pages/Home";
+import EventsPage from "./pages/Events";
+import EventDetailPage from "./pages/EventDetail";
+import NewEventPage from "./pages/NewEvent";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Homepage /> },
+  { path: "/events", element: <EventsPage /> },
+  { path: "events/:eventId", element: <EventDetailPage /> },
+  { path: "/events/new", element: <NewEventPage /> },
+]);
 
 function App() {
   return <div></div>;
