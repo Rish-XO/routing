00cdbce83e1ad5f1fb45a34521799420f597daf5
Rishal <Rishal.mhd009@gmail.com>
2023-06-01 16:59:45 +0530

    event view done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Homepage from "./pages/Home";
-import EventsPage, {loader as eventsLoader } from "./pages/Events";
-import EventDetailPage from "./pages/EventDetail";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
+import EventDetailPage, {
+  loader as eventDetailLoader,
+} from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
@@ -19,8 +21,12 @@ const router = createBrowserRouter([
         path: "events",
         element: <EventsRootLayout />,
         children: [
-          { index: true, element: <EventsPage />, loader: eventsLoader},
-          { path: ":eventId", element: <EventDetailPage /> },
+          { index: true, element: <EventsPage />, loader: eventsLoader },
+          {
+            path: ":eventId",
+            element: <EventDetailPage />,
+            loader: eventDetailLoader,
+          },
           { path: "new", element: <NewEventPage /> },
           { path: ":eventId/edit", element: <EditEventPage /> },
         ],
--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,14 +1,24 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { json, useLoaderData, useParams } from 'react-router-dom'
+import EventItem from '../components/EventItem'
 
 function EventDetailPage() {
-const params = useParams();
+const data = useLoaderData()
   return (
-    <div>
-      <h1>Event detail page</h1>
-      <p>Event ID: {params.eventId}</p>
-    </div>
-  )
+  <EventItem event={data.event}/>
+  );
 }
 
 export default EventDetailPage
+
+export async function loader({request, params}) {
+  const id = params.eventId
+const response = await fetch('http://localhost:8080/events/' +id)
+if(!response.ok) {
+  throw json({message: 'Could not fetch detail'}, {
+    status: 500
+  })
+} else {
+  return response;
+}
+}
