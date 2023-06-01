b1fbba27e11bf6d640f92d0c2ed8146607013ff5
Rishal <Rishal.mhd009@gmail.com>
2023-06-01 17:43:14 +0530

    to new event

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,24 +1,25 @@
-import React from 'react'
-import { json, useRouteLoaderData } from 'react-router-dom'
-import EventItem from '../components/EventItem'
+import React from "react";
+import { json, useRouteLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-const data = useRouteLoaderData('event-detail')
-  return (
-  <EventItem event={data.event}/>
-  );
+  const data = useRouteLoaderData("event-detail");
+  return <EventItem event={data.event} />;
 }
 
-export default EventDetailPage
+export default EventDetailPage;
 
-export async function loader({request, params}) {
-  const id = params.eventId
-const response = await fetch('http://localhost:8080/events/' +id)
-if(!response.ok) {
-  throw json({message: 'Could not fetch detail'}, {
-    status: 500
-  })
-} else {
-  return response;
-}
+export async function loader({ request, params }) {
+  const id = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + id);
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch detail" },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    return response;
+  }
 }
