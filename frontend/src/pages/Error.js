572f2d2fedeac4901198e92c5cb35b624d0aa162
Rishal <Rishal.mhd009@gmail.com>
2023-06-01 13:53:41 +0530

    error handling

---
+++ frontend/src/pages/Error.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PageContent from "../components/PageContent";
+
+function ErrorPage() {
+  return <PageContent title="An error occured">
+    <p>Something wente wrong!</p>
+  </PageContent>;
+}
+
+export default ErrorPage;
