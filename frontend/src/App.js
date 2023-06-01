e92ad89b36a78773c74fe55a2863244f39d8ffa0
Rishal <Rishal.mhd009@gmail.com>
2023-06-01 17:15:16 +0530

    event editing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,11 +24,17 @@ const router = createBrowserRouter([
           { index: true, element: <EventsPage />, loader: eventsLoader },
           {
             path: ":eventId",
-            element: <EventDetailPage />,
+            id: 'event-detail',
             loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+              },
+              { path: "edit", element: <EditEventPage /> },
+            ],
           },
           { path: "new", element: <NewEventPage /> },
-          { path: ":eventId/edit", element: <EditEventPage /> },
         ],
       },
     ],
