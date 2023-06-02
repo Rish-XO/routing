bcb0b51048b8a525ce5fdbeb8a5e9c164b122149
Rishal <Rishal.mhd009@gmail.com>
2023-06-02 22:45:15 +0530

    errir

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -50,7 +50,7 @@ function EventForm({ method, event }) {
         />
       </p>
       <p>
-        <label htmlFor="date">Date</label>
+        <lab el htmlFor="date">Date</label>
         <input
           id="date"
           type="date"
