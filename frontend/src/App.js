cd1d58a9b327e65c7f60ec33095c08b277077d4f
Rishal <Rishal.mhd009@gmail.com>
2023-06-02 22:44:46 +0530

    m

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -42,7 +42,7 @@ const router = createBrowserRouter([
             ],
           },
           {
-            path: "new",
+            path:  "new",
             element: <NewEventPage />,
             action: manipulateEventAction,
           },
